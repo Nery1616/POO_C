ebcb00feda008324667f7bec951fa4469111e3b8
neryvega16 <neryvegaso17@gmail.com>
2023-02-19 18:29:17 -0600

    primer_commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "cliente.cpp"
+#include <iostream>
+
+using namespace std;
+ main(){
+	string nit,nombres,apellidos,direccion;
+	int telefono;
+	
+	cout<<"Ingresar Nit: ";
+	cin>>nit;
+	cout<<"Ingresar Nombres: ";
+	cin>>nombres;
+	cout<<"Ingresar Apellidos: ";
+	cin>>apellidos;
+	cout<<"Ingresar Direccion: ";
+	cin>>direccion;
+	cout<<"Ingresar Telefono: ";
+	cin>>telefono;
+	//instancia de un objeto
+	Cliente obj = Cliente(nombres,apellidos,direccion,telefono,nit);
+	obj.mostrar();
+};
